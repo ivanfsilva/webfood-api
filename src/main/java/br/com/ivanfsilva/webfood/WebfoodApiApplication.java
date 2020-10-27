a79b22286f3fa99eb291c338b2aaedb90da4b4af
ivanfsilva <ivanfs27@gmail.com>
2020-10-27 11:04:22 -0300

    projeto criado

---
+++ src/main/java/br/com/ivanfsilva/webfood/WebfoodApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.ivanfsilva.webfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfoodApiApplication.class, args);
+	}
+
+}
