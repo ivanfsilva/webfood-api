de79e2c8babcedd06f94ada431e0d805a6669259
ivanfsilva <ivanfs27@gmail.com>
2020-11-05 14:15:55 -0300

    implementando interfaces repositories

---
+++ src/main/java/br/com/ivanfsilva/webfood/domain/repository/CidadeRepository.java
@@ -0,0 +1,13 @@
+package br.com.ivanfsilva.webfood.domain.repository;
+
+import java.util.List;
+
+import br.com.ivanfsilva.webfood.domain.model.Cidade;
+
+public interface CidadeRepository {
+	
+	List<Cidade> listar();
+	Cidade buscar(Long id);
+	Cidade salvar(Cidade cidade);
+	void remover(Cidade cidade);
+}
---
+++ src/main/java/br/com/ivanfsilva/webfood/domain/repository/PermissaoRepository.java
@@ -0,0 +1,14 @@
+package br.com.ivanfsilva.webfood.domain.repository;
+
+import java.util.List;
+
+import br.com.ivanfsilva.webfood.domain.model.Cidade;
+
+public interface PermissaoRepository {
+	
+	List<Cidade> listar();
+	Cidade buscar(Long id);
+	Cidade salvar(Cidade cidade);
+	void remover(Cidade cidade);
+
+}
