92c1d8067781599fff813cd8a3aefa1d022bed8e
ivanfsilva <ivanfs27@gmail.com>
2020-11-10 17:48:00 -0300

    implementando a inclusão Cozinha com POST

---
+++ src/main/java/br/com/ivanfsilva/webfood/api/controller/EstadoController.java
@@ -0,0 +1,25 @@
+package br.com.ivanfsilva.webfood.api.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.ivanfsilva.webfood.domain.model.Estado;
+import br.com.ivanfsilva.webfood.domain.repository.EstadoRepository;
+
+@RestController
+@RequestMapping("/estados")
+public class EstadoController {
+
+	@Autowired
+	private EstadoRepository estadoRepository;
+	
+	@GetMapping
+	public List<Estado> listar() {
+		return estadoRepository.listar();
+	}
+	
+}
