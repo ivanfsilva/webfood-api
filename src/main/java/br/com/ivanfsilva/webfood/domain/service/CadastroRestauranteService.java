28b85aece2775996c35355bc2f21537b07a5775b
ivanfsilva <ivanfs27@gmail.com>
2020-11-24 13:44:32 -0300

    implementando inclusão de restaurante

---
+++ src/main/java/br/com/ivanfsilva/webfood/domain/service/CadastroRestauranteService.java
@@ -0,0 +1,35 @@
+package br.com.ivanfsilva.webfood.domain.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.ivanfsilva.webfood.domain.exception.EntidadeNaoEncontradaException;
+import br.com.ivanfsilva.webfood.domain.model.Cozinha;
+import br.com.ivanfsilva.webfood.domain.model.Restaurante;
+import br.com.ivanfsilva.webfood.domain.repository.CozinhaRepository;
+import br.com.ivanfsilva.webfood.domain.repository.RestauranteRepository;
+
+@Service
+public class CadastroRestauranteService {
+
+	@Autowired
+	private RestauranteRepository restauranteRepository;
+	
+	@Autowired
+	private CozinhaRepository cozinhaRepository;
+	
+	public Restaurante salvar(Restaurante restaurante) {
+		Long cozinhaId = restaurante.getCozinha().getId();
+		Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
+		
+		if (cozinha == null) {
+			throw new EntidadeNaoEncontradaException(
+				String.format("Não existe cadastro de cozinha com código %d", cozinhaId));
+		}
+		
+		restaurante.setCozinha(cozinha);
+		
+		return restauranteRepository.salvar(restaurante);
+	}
+	
+}
