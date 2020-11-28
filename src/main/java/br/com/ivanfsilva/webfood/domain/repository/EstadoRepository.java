3cba0eabcf7815ec9af1ed8ce0beaa9acbf4796b
ivanfsilva <ivanfs27@gmail.com>
2020-11-28 17:43:56 -0300

    implementando EstadoRepository

--- src/main/java/br/com/ivanfsilva/webfood/domain/repository/EstadoRepository.java
+++ src/main/java/br/com/ivanfsilva/webfood/domain/repository/EstadoRepository.java
@@ -1,14 +1,11 @@
 package br.com.ivanfsilva.webfood.domain.repository;
 
-import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import br.com.ivanfsilva.webfood.domain.model.Cidade;
 import br.com.ivanfsilva.webfood.domain.model.Estado;
 
-public interface EstadoRepository {
-	
-	List<Estado> listar();
-	Estado buscar(Long id);
-	Estado salvar(Estado estado);
-	void remover(Long id);
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Long> {
+
 }
