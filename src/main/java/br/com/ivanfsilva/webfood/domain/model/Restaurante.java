4e1b84d876a823bd62fb80fc3ff852baae5957fb
ivanfsilva <ivanfs27@gmail.com>
2020-11-28 00:50:10 -0300

    atualização classes Restaurante e Cozinha

--- src/main/java/br/com/ivanfsilva/webfood/domain/model/Restaurante.java
+++ src/main/java/br/com/ivanfsilva/webfood/domain/model/Restaurante.java
@@ -1,12 +1,17 @@
 package br.com.ivanfsilva.webfood.domain.model;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
@@ -31,6 +36,13 @@ public class Restaurante {
 	private BigDecimal taxaFrete;
 	
 	@ManyToOne
+	@JoinColumn(name = "cozinha_id", nullable = false)
 	private Cozinha cozinha;
+	
+	@ManyToMany
+	@JoinTable(name = "restaurante_forma_agamento", 
+			joinColumns = @JoinColumn(name = "restaurante_id"), 
+			inverseJoinColumns = @JoinColumn(name = "forma_pagamento_id")) 
+	private List<FormaPagamento> formasPagamento = new ArrayList<>();
 
 }
