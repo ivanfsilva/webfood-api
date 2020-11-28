1ca773b9e5ad2a869105d31be5d872e544b44351
ivanfsilva <ivanfs27@gmail.com>
2020-11-28 18:04:51 -0300

    implementando CadastroEstadoService

--- src/main/java/br/com/ivanfsilva/webfood/domain/service/CadastroEstadoService.java
+++ src/main/java/br/com/ivanfsilva/webfood/domain/service/CadastroEstadoService.java
@@ -13,25 +13,25 @@ import br.com.ivanfsilva.webfood.domain.repository.EstadoRepository;
 @Service
 public class CadastroEstadoService {
 
-    @Autowired
-    private EstadoRepository estadoRepository;
-    
-    public Estado salvar(Estado estado) {
-        return estadoRepository.salvar(estado);
-    }
-    
-    public void excluir(Long estadoId) {
-        try {
-            estadoRepository.remover(estadoId);
-            
-        } catch (EmptyResultDataAccessException e) {
-            throw new EntidadeNaoEncontradaException(
-                String.format("Não existe um cadastro de estado com código %d", estadoId));
-        
-        } catch (DataIntegrityViolationException e) {
-            throw new EntidadeEmUsoException(
-                String.format("Estado de código %d não pode ser removido, pois está em uso", estadoId));
-        }
-    }
-    
+	@Autowired
+	private EstadoRepository estadoRepository;
+	
+	public Estado salvar(Estado estado) {
+		return estadoRepository.save(estado);
+	}
+	
+	public void excluir(Long estadoId) {
+		try {
+			estadoRepository.deleteById(estadoId);
+			
+		} catch (EmptyResultDataAccessException e) {
+			throw new EntidadeNaoEncontradaException(
+				String.format("Não existe um cadastro de estado com código %d", estadoId));
+		
+		} catch (DataIntegrityViolationException e) {
+			throw new EntidadeEmUsoException(
+				String.format("Estado de código %d não pode ser removido, pois está em uso", estadoId));
+		}
+	}
+	
 }
