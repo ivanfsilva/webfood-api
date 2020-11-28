ee61ec2c9f3aeb700d5e9cd5dcb4f245db0c85ac
ivanfsilva <ivanfs27@gmail.com>
2020-11-28 17:51:18 -0300

    implementando RestauranteRepository

--- src/main/java/br/com/ivanfsilva/webfood/domain/repository/RestauranteRepository.java
+++ src/main/java/br/com/ivanfsilva/webfood/domain/repository/RestauranteRepository.java
@@ -1,14 +1,39 @@
 package br.com.ivanfsilva.webfood.domain.repository;
 
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import br.com.ivanfsilva.webfood.domain.model.Restaurante;
 
-public interface RestauranteRepository {
+@Repository
+public interface RestauranteRepository 
+		extends CustomJpaRepository<Restaurante, Long>, RestauranteRepositoryQueries,
+		JpaSpecificationExecutor<Restaurante> {
 
-	List<Restaurante> listar();
-	Restaurante buscar(Long id);
-	Restaurante salvar(Restaurante restaurante);
-	void remover(Restaurante restaurante);
+	// Se um restaurante não tiver nenhuma forma de pagamento associada a ele,
+	// esse restaurante não será retornado usando JOIN FETCH r.formasPagamento.
+	// Para resolver isso, temos que usar LEFT JOIN FETCH r.formasPagamento
+//	@Query("from Restaurante r join fetch r.cozinha join fetch r.formasPagamento")
+	@Query("from Restaurante r join fetch r.cozinha left join fetch r.formasPagamento")
+	List<Restaurante> findAll();
+	
+	List<Restaurante> queryByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
+	
+//	@Query("from Restaurante where nome like %:nome% and cozinha.id = :id")
+	List<Restaurante> consultarPorNome(String nome, @Param("id") Long cozinha);
+	
+//	List<Restaurante> findByNomeContainingAndCozinhaId(String nome, Long cozinha);
+	
+	Optional<Restaurante> findFirstRestauranteByNomeContaining(String nome);
+	
+	List<Restaurante> findTop2ByNomeContaining(String nome);
+	
+	int countByCozinhaId(Long cozinha);
 	
 }
