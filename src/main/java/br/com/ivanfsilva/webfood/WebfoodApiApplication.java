6793face116415ff7afcbd2e17b5f7ca22619b21
ivanfsilva <ivanfs27@gmail.com>
2020-11-28 15:51:39 -0300

    configurando interfaces e app

--- src/main/java/br/com/ivanfsilva/webfood/WebfoodApiApplication.java
+++ src/main/java/br/com/ivanfsilva/webfood/WebfoodApiApplication.java
@@ -2,8 +2,12 @@ package br.com.ivanfsilva.webfood;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import br.com.ivanfsilva.webfood.infrastrutucture.repository.CustomJpaRepositoryImpl;
 
 @SpringBootApplication
+@EnableJpaRepositories(repositoryBaseClass = CustomJpaRepositoryImpl.class)
 public class WebfoodApiApplication {
 
 	public static void main(String[] args) {
