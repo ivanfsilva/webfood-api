01fe4fa11e8cf74554ce79c0dfd3a3d4e41a90d7
ivanfsilva <ivanfs27@gmail.com>
2021-04-24 21:53:38 -0300

    Adicionando endereço no modelo da representação do recurso de restaurante

---
+++ src/main/java/br/com/ivanfsilva/webfood/api/model/EnderecoModel.java
@@ -0,0 +1,17 @@
+package br.com.ivanfsilva.webfood.api.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class EnderecoModel {
+
+	private String cep;
+	private String logradouro;
+	private String numero;
+	private String complemento;
+	private String bairro;
+	private CidadeResumoModel cidade;
+	
+}
--- src/main/java/br/com/ivanfsilva/webfood/core/modelmapper/ModelMapperConfig.java
+++ src/main/java/br/com/ivanfsilva/webfood/core/modelmapper/ModelMapperConfig.java
@@ -4,12 +4,27 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import br.com.ivanfsilva.webfood.api.model.EnderecoModel;
+import br.com.ivanfsilva.webfood.domain.model.Endereco;
+
 @Configuration
 public class ModelMapperConfig {
 
 	@Bean
 	public ModelMapper modelMapper() {
-		return new ModelMapper();
+		var modelMapper = new ModelMapper();
+		
+//		modelMapper.createTypeMap(Restaurante.class, RestauranteModel.class)
+//			.addMapping(Restaurante::getTaxaFrete, RestauranteModel::setPrecoFrete);
+		
+		var enderecoToEnderecoModelTypeMap = modelMapper.createTypeMap(
+				Endereco.class, EnderecoModel.class);
+		
+		enderecoToEnderecoModelTypeMap.<String>addMapping(
+				enderecoSrc -> enderecoSrc.getCidade().getEstado().getNome(),
+				(enderecoModelDest, value) -> enderecoModelDest.getCidade().setEstado(value));
+		
+		return modelMapper;
 	}
 	
 }
