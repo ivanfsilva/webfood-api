d0d8433a9126a565177b503e006958f1264e4b56
ivanfsilva <ivanfs27@gmail.com>
2021-04-27 21:17:15 -0300

    implementando os endpoints de produtos

---
+++ src/main/java/br/com/ivanfsilva/webfood/api/assembler/ProdutoInputDisassembler.java
@@ -0,0 +1,23 @@
+package br.com.ivanfsilva.webfood.api.assembler;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import br.com.ivanfsilva.webfood.api.model.input.ProdutoInput;
+import br.com.ivanfsilva.webfood.domain.model.Produto;
+
+@Component
+public class ProdutoInputDisassembler {
+
+    @Autowired
+    private ModelMapper modelMapper;
+    
+    public Produto toDomainObject(ProdutoInput produtoInput) {
+        return modelMapper.map(produtoInput, Produto.class);
+    }
+    
+    public void copyToDomainObject(ProdutoInput produtoInput, Produto produto) {
+        modelMapper.map(produtoInput, produto);
+    }   
+}     
